f1c5f1e3432f248462155d59da26eaed1ea8b1a3
Oscar Roman <oscar.bastidas@creditek.com.co>
2021-11-18 11:28:11 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { Container, Box, Heading } from "@chakra-ui/react";
+
+const Page = () => {
+    return (
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello, I am a full stack developer based in Colombia!
+            </Box>
+            <Box display={{md: 'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page.title">
+                        Oscar Roman
+                    </Heading>
+                    <p>Digital Craftzman ( Developer )</p>
+                </Box>
+            </Box>
+        </Container>
+    )
+}
+
+export default Page
